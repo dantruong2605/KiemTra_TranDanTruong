094d2e19fd50bf8990bcb24121c4fc958b5e587a
dantruong2605 <106806607+dantruong2605@users.noreply.github.com>
2022-06-13 16:15:17 +0700

    Add project files.

---
+++ KiemTra_TranDanTruong/Controllers/SinhvienController.cs
@@ -0,0 +1,85 @@
+﻿using KiemTra_TranDanTruong.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KiemTra_TranDanTruong.Controllers
+{
+    public class SinhvienController : Controller
+    {
+        // GET: Sinhvien
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index()
+        {
+            var all_sinhvien = from tt in data.SinhViens select tt;
+            return View(all_sinhvien);
+        }
+
+        public ActionResult Detail(string id)
+        {
+            var D_Sinhvien = data.SinhViens.Where(m => m.MaSV == id).First();
+            return View(D_Sinhvien);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, SinhVien sv)
+        {
+            var ten = collection["HoTen"];
+            if (string.IsNullOrEmpty(ten))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                sv.HoTen = ten;
+                data.SinhViens.InsertOnSubmit(sv);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Create();
+        }
+        public ActionResult Edit(string id)
+        {
+            var E_category = data.SinhViens.First(m => m.MaSV == id);
+            return View(E_category);
+        }
+        [HttpPost]
+        public ActionResult Edit(string id, FormCollection collection)
+        {
+            var SinhVien = data.SinhViens.First(m => m.MaSV == id);
+            var E_HoTen = collection["tenloai"];
+            SinhVien.MaSV = id;
+            if (string.IsNullOrEmpty(E_HoTen))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                SinhVien.HoTen = E_HoTen;
+                UpdateModel(SinhVien);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Edit(id);
+        }
+        public ActionResult Delete(string id)
+        {
+            var D_Sinhvien = data.SinhViens.Where(m => m.MaSV == id).First();
+            return View(D_Sinhvien);
+        }
+        [HttpPost]
+        public ActionResult Delete(string id, FormCollection collection)
+        {
+            var D_Sinhvien = data.SinhViens.Where(m => m.MaSV == id).First();
+            data.SinhViens.DeleteOnSubmit(D_Sinhvien);
+            data.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ KiemTra_TranDanTruong/Models/Sinhvien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KiemTra_TranDanTruong.Models
+{
+    public class Sinhvien
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicYeaar { get; set; }
+        public double Price { get; set; }
+        public string Cover { get; set; }
+
+
+    }
+}
